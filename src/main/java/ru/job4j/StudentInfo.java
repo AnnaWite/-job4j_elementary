7c11ebae9be4f549b35641882b84cae7cd1d17c1
Анна <ana.al.kravchuk@gmail.com>
2022-02-19 22:27:50 +0700

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anna Anna");
+        System.out.println("22.02.1991");
+
+    }
+}
