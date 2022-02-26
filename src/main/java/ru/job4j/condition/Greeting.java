c476610a3299dc7fabfaeeeb3978b44acd246640
Анна <ana.al.kravchuk@gmail.com>
2022-02-26 16:53:04 +0700

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,13 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        String separate = " ";
+        String ideaPlusIdea = "But I am a newbie.";
+        int year = 2022;
+        String rsl = idea + separate + ideaPlusIdea + year;
+        System.out.println(rsl);
+
+    }
+}
