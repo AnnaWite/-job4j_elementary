7573fae8e85a06fe8905a4691c07ad9ed285625e
Анна <ana.al.kravchuk@gmail.com>
2022-02-26 17:13:17 +0700

    9.1. String [#237407]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,12 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        int x = x2 - x1;
-        int y = y2 - y1;
-        double rslX = Math.pow(x, 2);
-        double rslY = Math.pow(y, 2);
-        double rslXPlusRslY = rslX + rslY;
-        double rsl = Math.sqrt(rslXPlusRslY);
+        double rsl = Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
         return rsl;
     }
 
