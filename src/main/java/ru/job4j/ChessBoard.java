06161bafade2d89e02e78423a09e813447dceddc
Анна <ana.al.kravchuk@gmail.com>
2022-05-09 11:34:50 +0700

    4.3.6. Math.abs [#373616 #298821]

--- src/main/java/ru/job4j/ChessBoard.java
+++ src/main/java/ru/job4j/ChessBoard.java
@@ -8,7 +8,6 @@ public class ChessBoard {
             if ((x1 + y1) == (x2 + y2)
                     || Math.abs(x1 - y1) == Math.abs(x2 - y2)) {
                 rsl = Math.abs(x2 - x1);
-                rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
             }
         }
         return rsl;
